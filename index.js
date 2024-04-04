695404bc07d6b1c2c7530df49355187776735c35
100472037@alumnos.uc3m.es <Simpaticas_22>
2024-04-04 21:43:08 +0200

    nada

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+app.use('/', express.static(path.join(__dirname, 'www')));
+
+let clientSocket;
+
+io.on('connection', (socket) => {
+  console.log(`socket connected ${socket.id}`);
+
+  socket.on("POINTER_CONNECTED", () => {
+    socket.emit("ACK_CONNECTION");
+    if (clientSocket) clientSocket.emit("NEW_POINTER", { pointerId: socket.id });
+  });
+
+  socket.on("SENSOR_READING", (data) => {
+    //console.log(data);
+    if (clientSocket) clientSocket.emit("SENSOR_READING", {
+      pointerId: socket.id,
+      coords: data
+    });
+  });
+
+  socket.on("CLIENT_CONNECTED", () => {
+    clientSocket = socket;
+    clientSocket.emit("ACK_CONNECTION");
+  })
+});
+
+server.listen(8080, () => {
+  console.log("Server listening...");
+});
+
+/* const express = require('express');
+const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const fs = require('fs');
@@ -27,3 +63,4 @@ io.on("connection", function(socket){
 });
 
 server.listen(8080, () => console.log('server started'));
+ */
