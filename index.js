333891c61888eaa4554eed35d253cbe127cdd36b
100472037@alumnos.uc3m.es <Simpaticas_22>
2024-04-05 08:59:58 +0200

    socket cliente

--- index.js
+++ index.js
@@ -1,33 +1,20 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 const server = require('http').Server(app);
+
 const io = require('socket.io')(server);
 
-app.use('/', express.static(path.join(__dirname, 'www')));
+app.use(express.static('www'));
 
 let clientSocket;
 
-io.on('connection', (socket) => {
-  console.log(`socket connected ${socket.id}`);
+io.on('connection', function(socket){
+  console.log("Nuevo cliente conectado");
 
-  socket.on("POINTER_CONNECTED", () => {
-    socket.emit("ACK_CONNECTION");
-    if (clientSocket) clientSocket.emit("NEW_POINTER", { pointerId: socket.id });
+  socket.on("message_evt", function(message){
+    console.log(socket.id, message);
+    socket.broadcast.emit("message_evt", message);
   });
-
-  socket.on("SENSOR_READING", (data) => {
-    //console.log(data);
-    if (clientSocket) clientSocket.emit("SENSOR_READING", {
-      pointerId: socket.id,
-      coords: data
-    });
-  });
-
-  socket.on("CLIENT_CONNECTED", () => {
-    clientSocket = socket;
-    clientSocket.emit("ACK_CONNECTION");
-  })
 });
 
 server.listen(8080, () => {
